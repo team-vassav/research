722f9b1196be4d21414ea204038d43f77680a471
VASSAV <teamvassav@gmail.com>
2022-06-22 15:57:38 +0530

    Update research.py

--- research.py
+++ research.py
@@ -1,21 +1,17 @@
 import requests
-import streamlit as st
+#import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #import numpy as np
 
 while True:
 	r = requests.get('https://api.thingspeak.com/channels/1739457/feeds.csv?results=1')
-	#pprint.pprint(r.text)
 	contents = r.text
 	contents = contents.split("\n")
-	data = {}
-	for i in range(1,len(contents)-1):
-		points = contents[i][28:]
-		data[float(points[:3])] = float(points[4:])
-	#print(data)
-	data = pd.DataFrame(list(data.items()),columns = ['column1','column2']) 
+	x,y = contents[1][28:].split(',')
+	'''f = plt.figure()
+	f.set_figwidth(4)
+	f.set_figheight(1)'''
 	
-	#st.dataframe(data[:10])
-		
-	st.line_chart(data)
+	plt.scatter(float(x), float(y), linewidth=4.0)
+	plt.show()
