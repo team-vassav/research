9399d3e84876da547a27ba1aeca65e8052bf84e9
VASSAV <teamvassav@gmail.com>
2022-06-23 09:46:24 +0530

    Update research.py

--- research.py
+++ research.py
@@ -1,11 +1,10 @@
 import requests
 import streamlit as st
-import pandas as pd
 import matplotlib.pyplot as plt
-#import numpy as np
 import time
 
 fig, ax = plt.subplots()
+st.pyplot(fig)
 
 while True:
 	r = requests.get('https://api.thingspeak.com/channels/1739457/feeds.csv?results=1')
@@ -13,5 +12,4 @@ while True:
 	contents = contents.split("\n")
 	x,y = contents[1][28:].split(',')
 	plt.scatter(float(x), float(y), linewidth=4.0)
-	st.pyplot(fig)
 	time.sleep(5)
