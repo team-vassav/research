c2976b5239d23996c54bb810826d9103b53dc606
VASSAV <teamvassav@gmail.com>
2022-06-23 09:45:04 +0530

    Update research.py

--- research.py
+++ research.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 #import numpy as np
 import time
 
+fig, ax = plt.subplots()
+
 while True:
 	r = requests.get('https://api.thingspeak.com/channels/1739457/feeds.csv?results=1')
 	contents = r.text
 	contents = contents.split("\n")
 	x,y = contents[1][28:].split(',')
-	fig, ax = plt.subplots()
 	plt.scatter(float(x), float(y), linewidth=4.0)
 	st.pyplot(fig)
 	time.sleep(5)
