7b9e9e69f590b02595f4419e120dea4c071e1b1d
VASSAV <teamvassav@gmail.com>
2022-06-23 21:17:09 +0530

    Update research.py

--- research.py
+++ research.py
@@ -1,53 +1,45 @@
-import requests
-import streamlit as st
 import matplotlib.pyplot as plt
-#import matplotlib.animation as animation
-
-import time
-from random import randint
+import numpy as np
 from matplotlib.animation import FuncAnimation
+import psutil
+import collections
+import requests
 
-'''while True:
-	r = requests.get('https://api.thingspeak.com/channels/1739457/feeds.csv?results=1')
-	contents = r.text
-	contents = contents.split("\n")
-	x,y = contents[1][28:].split(',')
-	plt.scatter(float(x), float(y), linewidth=4.0)
-	time.sleep(5)
-	st.pyplot(fig=plt, clear_figure=True)
-ani = animation.FuncAnimation(fig, animate, interval=1000)'''
 
-'''x = []
-y = []
-
-fig, ax = plt.subplots()
-
-def animate(i):
-    pt = randint(1,9)
-    x.append(i)
-    y.append(pt)
-
-    ax.clear()
-    ax.plot(x, y)
-    ax.set_xlim([0,100])
-    ax.set_ylim([0,10])
-
-ani = FuncAnimation(fig, animate, frames=100, interval=500, repeat=False)
-
-plt.show()'''
-
-x=1
-x_data=[1]
-y_data = []
-plt.ylim(1,8)
-plt.xlim(1,8)
-
-while True:
-	y_data = y_data.append(randint(1,9))
-	x += 1
-	x_data.append(x)
+def my_function(i):
+    r = requests.get('https://api.thingspeak.com/channels/1739457/feeds.csv?results=1')
+    contents = r.text
+    contents = contents.split("\n")
+    x,y = contents[1][28:].split(',')
+    # get data
+    cpu.popleft()
+    cpu.append(float(x))
+    ram.popleft()
+    ram.append(float(y))
+    # clear axis
+    ax.cla()
+    ax1.cla()
+    # plot cpu
+    ax.plot(cpu)
+    ax.scatter(len(cpu) - 1, cpu[-1])
+    ax.text(len(cpu) - 1, cpu[-1] + 2, "{}%".format(cpu[-1]))
+    ax.set_ylim(0, 100)
+    # plot memory
+    ax1.plot(ram)
+    ax1.scatter(len(ram) - 1, ram[-1])
+    ax1.text(len(ram) - 1, ram[-1] + 2, "{}%".format(ram[-1]))
+    ax1.set_ylim(0, 100)
 	
-	plt.plot(x_data, y_data, color='green', linestyle='dashed', linewidth = 3,
-         marker='o', markerfacecolor='blue', markersize=12)
-	time.sleep(1)
-	st.pyplot(fig=plt, clear_figure=True)
+
+# start collections with zeros
+cpu = collections.deque(np.zeros(10))
+ram = collections.deque(np.zeros(10))
+# define and adjust figure
+fig = plt.figure(figsize=(12, 6), facecolor='#DEDEDE')
+ax = plt.subplot(121)
+ax1 = plt.subplot(122)
+ax.set_facecolor('#DEDEDE')
+ax1.set_facecolor('#DEDEDE')
+# animate
+ani = FuncAnimation(fig, my_function, interval=6000)
+plt.show()
