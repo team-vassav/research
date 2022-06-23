9ba080933d051705ab0ae1f39c2712647feacfa3
VASSAV <teamvassav@gmail.com>
2022-06-23 09:50:24 +0530

    Update research.py

--- research.py
+++ research.py
@@ -1,15 +1,22 @@
 import requests
 import streamlit as st
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 import time
 
-fig, ax = plt.subplots()
-st.pyplot(fig)
+style.use('fivethirtyeight')
 
-while True:
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate():
 	r = requests.get('https://api.thingspeak.com/channels/1739457/feeds.csv?results=1')
 	contents = r.text
 	contents = contents.split("\n")
 	x,y = contents[1][28:].split(',')
 	plt.scatter(float(x), float(y), linewidth=4.0)
 	time.sleep(5)
+
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
