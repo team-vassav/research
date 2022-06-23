8de851ac984c47d016d91aaac876faa2d0c3a7e9
VASSAV <teamvassav@gmail.com>
2022-06-23 09:53:17 +0530

    Update research.py

--- research.py
+++ research.py
@@ -10,13 +10,12 @@ style.use('fivethirtyeight')
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 
-def animate():
+while True:
 	r = requests.get('https://api.thingspeak.com/channels/1739457/feeds.csv?results=1')
 	contents = r.text
 	contents = contents.split("\n")
 	x,y = contents[1][28:].split(',')
 	plt.scatter(float(x), float(y), linewidth=4.0)
 	time.sleep(5)
-
-ani = animation.FuncAnimation(fig, animate, interval=1000)
-plt.show()
+	st.pyplot(fig=plt, clear_figure=True)
+#ani = animation.FuncAnimation(fig, animate, interval=1000)
