1987ad0721d7ccd0bb35d6d0a3f7213cf26887fe
VASSAV <teamvassav@gmail.com>
2022-06-23 22:18:54 +0530

    Update research.py

--- research.py
+++ research.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
-import psutil
 import collections
 import requests
-
+import streamlit as st
 
 def my_function(i):
-    r = requests.get('https://api.thingspeak.com/channels/1739457/feeds.csv?results=1')
+    r = requests.get(
+        'https://api.thingspeak.com/channels/1739457/feeds.csv?results=1')
     contents = r.text
     contents = contents.split("\n")
-    x,y = contents[1][28:].split(',')
+    x, y = contents[1][28:].split(',')
     # get data
     cpu.popleft()
     cpu.append(float(x))
@@ -23,13 +23,13 @@ def my_function(i):
     ax.plot(cpu)
     ax.scatter(len(cpu) - 1, cpu[-1])
     ax.text(len(cpu) - 1, cpu[-1] + 2, "{}%".format(cpu[-1]))
-    ax.set_ylim(0, 100)
+    ax.set_ylim(0, 10)
     # plot memory
     ax1.plot(ram)
     ax1.scatter(len(ram) - 1, ram[-1])
     ax1.text(len(ram) - 1, ram[-1] + 2, "{}%".format(ram[-1]))
-    ax1.set_ylim(0, 100)
-	
+    ax1.set_ylim(0, 10)
+
 
 # start collections with zeros
 cpu = collections.deque(np.zeros(10))
@@ -41,5 +41,6 @@ ax1 = plt.subplot(122)
 ax.set_facecolor('#DEDEDE')
 ax1.set_facecolor('#DEDEDE')
 # animate
-ani = FuncAnimation(fig, my_function, interval=6000)
-st.pyplot(fig)
+ani = FuncAnimation(fig, my_function, interval=1000)
+plt.show()
+st.pyplot()
