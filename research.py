8ad2778cec9e27f8bc43bf682badaf8d553dd0a4
VASSAV <teamvassav@gmail.com>
2022-06-26 15:33:29 +0530

    Update research.py

--- research.py
+++ research.py
@@ -1,55 +1,26 @@
+import streamlit as st
+import time
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.animation import FuncAnimation
-import collections
-import requests
-import streamlit as st
+from collections import deque
+fig, ax = plt.subplots()
 
-def my_function(i):
-    r = requests.get(
-        'https://api.thingspeak.com/channels/1739457/feeds.csv?results=1')
-    contents = r.text
-    contents = contents.split("\n")
-    x, y = contents[1][28:].split(',')
-    # get data
-    cpu.popleft()
-    cpu.append(float(x))
-    #ram.popleft()
-    #ram.append(float(y))
-    # clear axis
-    ax.cla()
-    #ax1.cla()
-    # plot cpu
-    ax.plot(cpu)
-    ax.scatter(len(cpu) - 1, cpu[-1])
-    ax.text(len(cpu) - 1, cpu[-1] + 2, "{}%".format(cpu[-1]))
-    ax.set_ylim(0, 10)
-    # plot memory
-    #ax1.plot(ram)
-    #ax1.scatter(len(ram) - 1, ram[-1])
-    #ax1.text(len(ram) - 1, ram[-1] + 2, "{}%".format(ram[-1]))
-    #ax1.set_ylim(0, 10)
+max_samples = 100
+max_x = max_samples
+max_rand = 100
 
+x = np.arange(0, max_x)
+y = deque(np.zeros(max_samples), max_samples)
 
-# start collections with zeros
-cpu = collections.deque(np.zeros(10))
-#ram = collections.deque(np.zeros(10))
-# define and adjust figure
-fig = plt.figure(figsize=(12, 6), facecolor='#DEDEDE')
-ax = plt.subplots()
-#ax1 = plt.subplot(122)
-#ax.set_facecolor('#DEDEDE')
-#ax1.set_facecolor('#DEDEDE')
-# animate
-while True:
-    ani = FuncAnimation(fig, my_function, interval=1000)
-    #my_function()
-    #plt.show()
-    st.pyplot(fig,clear_figure=True)
+ax.set_ylim(0, max_rand)
+line, = ax.plot(x, np.array(y))
+the_plot = st.pyplot(plt)
 
+def animate():  # update the y values (every 1000ms)
+    line.set_ydata(np.array(y))
+    the_plot.pyplot(plt)
+    y.append(np.random.randint(max_x)) #append y with a random integer between 0 to 100
 
-#arr = np.random.normal(1, 1, size=100)
-#fig, ax = plt.subplots()
-#ax.hist(arr, bins=20)
-
-#st.pyplot(fig)
+for i in range(200):
+    animate()
+    time.sleep(0.01)
