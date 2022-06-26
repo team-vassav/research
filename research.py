dc4735d5131a4b9c7fec7b8e61017c756b02c9cf
VASSAV <teamvassav@gmail.com>
2022-06-26 14:33:26 +0530

    Update research.py

--- research.py
+++ research.py
@@ -14,33 +14,34 @@ def my_function(i):
     # get data
     cpu.popleft()
     cpu.append(float(x))
-    ram.popleft()
-    ram.append(float(y))
+    #ram.popleft()
+    #ram.append(float(y))
     # clear axis
     ax.cla()
-    ax1.cla()
+    #ax1.cla()
     # plot cpu
     ax.plot(cpu)
     ax.scatter(len(cpu) - 1, cpu[-1])
     ax.text(len(cpu) - 1, cpu[-1] + 2, "{}%".format(cpu[-1]))
     ax.set_ylim(0, 10)
     # plot memory
-    ax1.plot(ram)
-    ax1.scatter(len(ram) - 1, ram[-1])
-    ax1.text(len(ram) - 1, ram[-1] + 2, "{}%".format(ram[-1]))
-    ax1.set_ylim(0, 10)
+    #ax1.plot(ram)
+    #ax1.scatter(len(ram) - 1, ram[-1])
+    #ax1.text(len(ram) - 1, ram[-1] + 2, "{}%".format(ram[-1]))
+    #ax1.set_ylim(0, 10)
 
 
 # start collections with zeros
 cpu = collections.deque(np.zeros(10))
-ram = collections.deque(np.zeros(10))
+#ram = collections.deque(np.zeros(10))
 # define and adjust figure
 fig = plt.figure(figsize=(12, 6), facecolor='#DEDEDE')
 ax = plt.subplot(121)
-ax1 = plt.subplot(122)
+#ax1 = plt.subplot(122)
 ax.set_facecolor('#DEDEDE')
-ax1.set_facecolor('#DEDEDE')
+#ax1.set_facecolor('#DEDEDE')
 # animate
-ani = FuncAnimation(fig, my_function, interval=1000)
-plt.show()
-st.pyplot()
+while True:
+    ani = FuncAnimation(fig, my_function, interval=1000)
+    #my_function()
+    plt.show()
