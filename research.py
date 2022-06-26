383c6ed2e4766be3fab3f81281d34e317cb90010
VASSAV <teamvassav@gmail.com>
2022-06-26 15:47:22 +0530

    Update research.py

--- research.py
+++ research.py
@@ -1,8 +1,11 @@
+
 import streamlit as st
 import time
+import requests
 import matplotlib.pyplot as plt
 import numpy as np
 from collections import deque
+
 fig, ax = plt.subplots()
 
 max_samples = 100
@@ -19,7 +22,12 @@ the_plot = st.pyplot(plt)
 def animate():  # update the y values (every 1000ms)
     line.set_ydata(np.array(y))
     the_plot.pyplot(plt)
-    y.append(np.random.randint(max_x)) #append y with a random integer between 0 to 100
+    r = requests.get('https://api.thingspeak.com/channels/1739457/feeds.csv?results=1')
+    contents = r.text
+    contents = contents.split("\n")
+    x_p, y_p = contents[1][28:].split(',')
+    y.append(float(y_p))
+	#y.append(np.random.randint(max_x)) #append y with a random integer between 0 to 100
 
 for i in range(200):
     animate()
